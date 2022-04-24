0a2ce0da08a70aa34874c7dd0a8b6798af6b38b5
ali <ali.khosravani@gmail.coom>
2022-04-24 19:26:32 +0430

    Add js

--- Layout/topBaner.php
+++ Layout/topBaner.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
  echo '   <div style="height:45px;width:100%;text-align:center;background-color:darkblue;color:white;margin:0;line-height: 2.6;">';
  echo '   <p class="topbanner">  به فروشگاه اینترنتی همواره تخفیف خوش آمدید</p>';
@@ -8,3 +17,10 @@
 
 
 ?>
+
+
+
+
+
+
+
