010480d1e6b50972fc8b98a6fd62d9d0fc12c84f
ali <ali.khosravani@gmail.coom>
2022-04-26 00:48:27 +0430

    Home page

--- Index.php
+++ Index.php
@@ -8,13 +8,14 @@
     <title>Document</title>
 </head>
 <body>
-
+<div>
 <?php
     include("Layout/topBanner.php");
     ?>
     <?php
     include("Layout/header.php");
       ?>
+</div>
 <div>
 
   <div style="width:100%;margin:0;padding:0;">
@@ -22,6 +23,45 @@
     include("Layout/banner.php");
     ?>
   </div>
+  <div class="row">
+  <div class="col">col</div>
+  <div class="col">col</div>
+  <div class="col">col</div>
+  <div class="col">col</div>
+</div>
+<div style="height:250px;background-color:darkblue;width:85%;margin:30px auto;border-radius:30px;"></div>
+<div style="height:100px;background-color:lightblue;width:75%;margin:20px auto;border-radius:30px;"></div>
+<div style="height:100px;background-color:blue;width:75%;margin:20px auto;border-radius:30px;"></div>
+
+<div class="row">
+    <div class="col-sm-3" style="background-color:lavender;height:250px">.col-sm-3</div>
+    <div class="col-sm-3" style="background-color:lavenderblush;height:250px">.col-sm-3</div>
+    <div class="col-sm-3" style="background-color:lavender;height:250px">.col-sm-3</div>
+    <div class="col-sm-3" style="background-color:lavenderblush;height:250px">.col-sm-3</div>
+  </div>
+
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">دسته بندی فروشگاه اینترنتی</h1>
+    <div class="row">
+    <div class="col-sm-3" style="background-color:lavender;height:150px">.col-sm-3</div>
+    <div class="col-sm-3" style="background-color:lavenderblush;height:150px">.col-sm-3</div>
+    <div class="col-sm-3" style="background-color:lavender;height:150px">.col-sm-3</div>
+    <div class="col-sm-3" style="background-color:lavenderblush;height:150px">.col-sm-3</div>
+  </div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="row">
+    <div class="col">
+      1 of 2
+    </div>
+    <div class="col">
+      2 of 2
+    </div>
+  </div>
+</div>
 
 </div>
       <?php     
